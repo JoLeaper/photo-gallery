4351c8a6caf713bd2fea59c9f4da59eabca67ef4
JoLeaper <joleap123@gmail.com>
2020-04-28 17:23:21 -0700

    added search by keywords

--- src/App.js
+++ src/App.js
@@ -7,22 +7,35 @@ import hornData from './data.js'
 
 export default class App extends Component {
   state = {
-    horns: 1
+    horns: 1,
+    keyword: ''
   };
 
   handleChange = (event) => {
-    console.log(event.target.value);
     this.setState({
-      horns: event.target.value
+            horns: event.target.value
     });
   }
+
+  handleInputChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      keyword: event.target.value
+    })
+  }
+
+
   render() {
     const filteredCreatures = hornData
     .filter(item => item.horns >= this.state.horns)
+    .filter(item => item.keyword.includes(this.state.keyword))
 
     return (
       <div>
         <Header />
+        <input value={this.state.keyword} onChange={this.handleInputChange}>
+
+        </input>
         <select onChange={this.handleChange}>
           <option value="1">One Horn</option>
           <option value="2">Two Horns</option>
